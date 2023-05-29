753c01446298abd259ec0e24de9e739288451e3f
Alexander <ai5517085@gmail.com>
2023-05-29 20:16:40 +0300

    Initial commit

---
+++ src/main/java/com/example/bigjavahomework/BigJavaHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.bigjavahomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigJavaHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigJavaHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bigjavahomework/BigJavaHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bigjavahomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigJavaHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
